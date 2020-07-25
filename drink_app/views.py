c07dcdec0eca8880bb0f12bf331b94714dbfc0a2
Matthew Carrano <mc25573@gmail.com>
2020-07-24 22:31:49 -0500

    Drinks display with styling. Clicking them does nothing.

--- drink_app/views.py
+++ drink_app/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import Drink
+from django.db.models import Q
 
 def home(request):
-    return render(request,'drink_app/home.html')
+    
+    drinks = Drink.objects.filter(mix_type__exact='shaken'
+                                 ).filter(glass__exact='Highball glass'
+                                 ).order_by('name')
+    #Q(mix_type__exact='shaken')|Q(mix_type__exact='stirred')|Q(mix_type__exact='shot')|Q(mix_type__exact='tea')
+    
+    context = {
+        'drinks': drinks # the key is what is used in templates           
+        }
+    
+    return render(request,'drink_app/home.html',context)
