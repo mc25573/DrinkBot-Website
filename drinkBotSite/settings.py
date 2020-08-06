6f35b333df8a6846671984cde60fd63a4657173d
Matthew Carrano <mc25573@gmail.com>
2020-08-06 01:14:33 -0500

    Made it clear I'm not using the production secret in previous commits

--- drinkBotSite/settings.py
+++ drinkBotSite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '75_x@6j%f%j^wl!!_8zj@tb#3ra1-)tss^o4mn9ixsbfxcjsh4'
+SECRET_KEY = 'not_necessary'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
