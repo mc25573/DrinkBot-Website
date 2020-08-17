28788634b8560cbb3c2351fda9137b9f1ea9e23e
Matthew Carrano <mc25573@gmail.com>
2020-08-17 18:00:17 -0500

    Added new ip to settings

--- drinkBotSite/settings.py
+++ drinkBotSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'not_necessary'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.69','127.0.0.1','192.168.1.85']
+ALLOWED_HOSTS = ['192.168.1.69','127.0.0.1','192.168.1.85','192.168.3.15']
 
 
 # Application definition
